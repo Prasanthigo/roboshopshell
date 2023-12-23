3af9aa5d45c8a32054e75fb8327535812fa60ee2
Prasanthi Gogineni <gprashanthi.dev@gmail.com>
2023-12-22 22:50:37 -0600

    committed shipping

---
+++ shipping.sh
@@ -0,0 +1,65 @@
+DATE=$(date +%F)
+LOGS_DIR=/tmp
+SCRIPT_NAME=$0
+LOGFILE=$LOGS_DIR/$SCRIPT_NAME-$DATE.log
+RED="\e[31m"
+GREEN="\e[32m"
+ENDCOLOR="\e[0m"
+USERID=$(id -u)
+if [ "$USERID" -ne 0 ]; then
+    echo -e "$RED ERROR: Please execute the script with root permissions.$ENDCOLOR"
+    exit 1
+fi
+VALIDATE()
+{
+    if [ $1 -ne 0 ];
+    then
+        echo -e "$2 ..........$RED failed $ENDCOLOR"
+    else
+        echo -e "$2 ...........$GREEN success $ENDCOLOR"
+    fi
+}
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>> $LOGFILE
+VALIDATE $? "setting up NPM source"
+yum install nodejs -y &>> $LOGFILE
+VALIDATE $? "Installing Nodejs"
+USER_EXISTS=$(cat /etc/passwd | grep "roboshop")
+if [ $? -ne 0 ]
+then
+    useradd roboshop
+else
+    echo "user already exists"
+fi
+if [ -d /app ]
+then
+    echo "directory already exists"
+else
+    mkdir /app
+    echo "directory created"
+fi
+yum install maven -y &>> $LOGFILE
+VALIDATE $? "installing maven"
+curl -L -o /tmp/shipping.zip https://roboshop-builds.s3.amazonaws.com/shipping.zip &>> $LOGFILE
+VALIDATE $? "Downloading application code to /tmp"
+cd /app &>> $LOGFILE
+VALIDATE $? "changing to app dir"
+unzip -o /tmp/shipping.zip &>> $LOGFILE
+VALIDATE $? "unzipping user zip file"
+mvn clean package &>> $LOGFILE
+VALIDATE $? "building the app"
+mv target/shipping-1.0.jar shipping.jar &>> $LOGFILE
+VALIDATE $? "moving jar file"
+cp /home/centos/roboshopshell/shipping.service /etc/systemd/system/shipping.service &>> $LOGFILE
+VALIDATE $? "copying service file"
+systemctl daemon-reload &>> $LOGFILE
+VALIDATE $? "reloading the service"
+systemctl enable shipping &>> $LOGFILE
+VALIDATE $? "enabling the service"
+systemctl start shipping &>> $LOGFILE
+VALIDATE $? "starting the service"
+yum install mysql -y &>> $LOGFILE
+VALIDATE $? "installing my-sql"
+mysql -h mysql.joindevops.pro -uroot -pRoboShop@1 < /app/schema/shipping.sql &>> $LOGFILE
+VALIDATE $? "loading schema"
+systemctl restart shipping &>> $LOGFILE
+VALIDATE $? "Restarting shipping"
