a47443fcc4682e68751144e6a7a014ad19ac566f
Prasanthi Gogineni <gprashanthi.dev@gmail.com>
2023-12-24 11:16:50 -0600

    committing create_instance

---
+++ Instance_creation.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+NAMES=("Mongodb" "web" "Catalogue" "Cart" "Shipping" "User" "Mysql" "Redis" "RabbitMq" "Dispatch" "Payment")
+Instance_Type=""
+Security_Group_Id="sg-03385513c7e65e046"
+Image_Id="ami-03265a0778a880afb"
+Ip_Address=""
+for i in $(NAMES[@])
+do
+    if [[ $i==Mongodb || $i==Mysql ]];
+    then
+        Instance_Type="t3.medium"
+    else
+        Instance_Type="t2.micro"
+    echo "creating $i instance"
+    Ip_Address=$(aws ec2 run-instances \
+    --image-id $Image_Id \
+    --instance-type $Instance_Type \
+    --security-group-ids $Security_Group_Id \
+    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$i}]') |
+    jq -r '.instances[0].PrivateIpAddress')
+done
+
